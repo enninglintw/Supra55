a3c5ce5356e598f58ed60441bead0b6432e7923a
playpool513 <playpool513@gmail.com>
2014-09-17 16:55:51 +0800

    MVC for members#new, create

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,26 @@
 class MembersController < ApplicationController
+
+  def new
+    @org = Org.find(params[:org_id])
+    @member = @org.members.new
+  end
+
+  def create
+    @org = Org.find(params[:org_id])
+    @member = @org.members.new(member_params)
+
+    if @member.save
+      redirect_to org_path(@org), :notice => '新增使用者成功！'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :tel, :education_permitted, :educated_for_sei, :license_for_sei)
+  end
+
 end
