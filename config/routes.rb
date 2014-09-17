ed22f6f22b15769654a11915e18398b4cb56f377
playpool513 <playpool513@gmail.com>
2014-09-17 10:52:00 +0800

    routes.rb - root & path for orgs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :orgs
+
+  root 'orgs#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
