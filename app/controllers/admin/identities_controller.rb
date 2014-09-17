70cdfdb633ea304dc47e5709e8f2e00246692b50
playpool513 <playpool513@gmail.com>
2014-09-18 00:41:17 +0800

    MVC for admin/identities#new, create

--- app/controllers/admin/identities_controller.rb
+++ app/controllers/admin/identities_controller.rb
@@ -1,5 +1,34 @@
 class Admin::IdentitiesController < ApplicationController
+
   def index
     @identities = Identity.all
+  end
+
+  def new
+    @identity = Identity.new
+  end
+
+  def create
+    @identity = Identity.new(identity_params)
+
+    if @identity.save
+      redirect_to admin_identities_path, :notice => '新增分類成功！'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def identity_params
+    params.require(:identity).permit(:classification, 
+                                     :sei_fee, 
+                                     :sei_eds_fee, 
+                                     :sei_ebsd_fee, 
+                                     :sei_eds_ebsd_fee, 
+                                     :discount_above_30k, 
+                                     :discount_above_40k, 
+                                     :discount_above_60k)
   end
 end
