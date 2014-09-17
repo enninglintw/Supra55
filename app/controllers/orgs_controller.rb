175413859613632d1002d6a15fbd51fe628dcc2a
playpool513 <playpool513@gmail.com>
2014-09-17 11:39:07 +0800

    MVC for orgs#new, create

--- app/controllers/orgs_controller.rb
+++ app/controllers/orgs_controller.rb
@@ -4,4 +4,25 @@ class OrgsController < ApplicationController
     @orgs = Org.all
   end
 
+  def new
+    @org = Org.new
+  end
+
+  def create
+    @org = Org.new(org_params)
+
+    if @org.save
+      redirect_to orgs_path, :notice => '新增使用單位成功！'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def org_params
+    params.require(:org).permit(:name, :official_name, :address, :tel)
+  end
+
 end
