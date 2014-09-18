decc80f5a8213dfa69299fb94561afaf1bfb0341
playpool513 <playpool513@gmail.com>
2014-09-18 17:32:57 +0800

    MVC for admin/members#index

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,2 +1,10 @@
 class Admin::MembersController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :admin_required
+
+  def index
+    @members = Member.all
+  end
+  
 end
