f3a914c384b2b709ed7c889ec62fbfd08d96938d
playpool513 <playpool513@gmail.com>
2014-09-18 17:48:44 +0800

    MVC for admin/members#new, create

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -6,5 +6,32 @@ class Admin::MembersController < ApplicationController
   def index
     @members = Member.all
   end
-  
+
+  def new
+    @member = Member.new
+    @options = Org.all.collect do |org|
+      [org.name, org.id]
+    end
+  end
+
+  def create
+    @member = Member.new(member_params)
+
+    if @member.save
+      redirect_to admin_members_path, :notice => '新增使用者成功！'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name,
+                                    :tel,
+                                    :education_permitted,
+                                    :educated_for_sei,
+                                    :license_for_sei)
+  end
 end
