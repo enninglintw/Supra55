6e99e4003d8a5b0e8f1ead266829eff8595a9b7e
playpool513 <playpool513@gmail.com>
2014-09-18 23:34:14 +0800

    $ rails g controller admin::record

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :identities
     resources :orgs
     resources :members
+    resources :records
   end
 
   devise_for :users
@@ -12,7 +13,7 @@ Rails.application.routes.draw do
     resources :members
   end
 
-  root 'admin/orgs#index'
+  root 'admin/identities#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/helpers/admin/record_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class Admin::RecordHelperTest < ActionView::TestCase
-end
