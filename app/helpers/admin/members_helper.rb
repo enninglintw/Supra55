737095b822fbc35753e30144a8c509fd897a6600
playpool513 <playpool513@gmail.com>
2014-09-22 15:19:44 +0800

    def hide_member_name(member)

--- app/helpers/admin/members_helper.rb
+++ app/helpers/admin/members_helper.rb
@@ -1,2 +1,22 @@
 module Admin::MembersHelper
+
+  def hide_member_name(member)
+    # member.name.gsub(member.name, "***")
+    if member.name.size > 4
+      member.name.gsub(/([a-z])+/, '*')
+    elsif member.name.size == 4
+      member.name[0,1] + 
+      member.name[1,1].gsub(/./, '*') +
+      member.name[2,1] +
+      member.name[3,1].gsub(/./, '*')
+    elsif member.name.size == 3
+      member.name[0,1] + 
+      member.name[1,1].gsub(/./, '*') +
+      member.name[2,1]
+    elsif member.name.size == 2
+      member.name[0,1] + 
+      member.name[1,1].gsub(/./, '*')
+    end
+  end
+
 end
