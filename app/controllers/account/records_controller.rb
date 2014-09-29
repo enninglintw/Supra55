e66226cbbbffd052cac786a5e2b5b745eec2ecf0
playpool513 <playpool513@gmail.com>
2014-09-29 14:28:57 +0800

    MVC for account/members#show and account/records#index

--- app/controllers/account/records_controller.rb
+++ app/controllers/account/records_controller.rb
@@ -1,2 +1,17 @@
 class Account::RecordsController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :find_org
+
+  def index
+    @records = @org.records.order("start_at")
+  end
+
+
+  private
+
+  def find_org
+    @org = Org.find_by(id: current_user.member.org_id)
+  end
+
 end
