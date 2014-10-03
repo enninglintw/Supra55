4932e98814cd0cb99ec4038c5e63223d5728fb17
playpool513 <playpool513@gmail.com>
2014-10-03 22:47:20 +0800

    hide other members' name in account views

--- app/helpers/account/members_helper.rb
+++ app/helpers/account/members_helper.rb
@@ -1,2 +1,25 @@
 module Account::MembersHelper
+
+  def hide_member_name_from_members(member)
+    # FIXME: hide part of the infos, rewrite the code
+    # FIXME: ask if this can be writen in model
+    member.name.gsub(member.name, "***")
+    # member.name.gsub(/([a-z])+/, '*')
+    # if member.name.size > 4
+    #   member.name.gsub(/([a-z])+/, '*')
+    # elsif member.name.size == 4
+    #   member.name[0,1] + 
+    #   member.name[1,1].gsub(/./, '*') +
+    #   member.name[2,1] +
+    #   member.name[3,1].gsub(/./, '*')
+    # elsif member.name.size == 3
+    #   member.name[0,1] + 
+    #   member.name[1,1].gsub(/./, '*') +
+    #   member.name[2,1]
+    # elsif member.name.size == 2
+    #   member.name[0,1] + 
+    #   member.name[1,1].gsub(/./, '*')
+    # end
+  end
+
 end
--- app/helpers/account/records_helper.rb
+++ app/helpers/account/records_helper.rb
@@ -1,2 +1,25 @@
 module Account::RecordsHelper
+
+  def hide_member_name_from_records(record)
+    # FIXME: hide part of the infos, rewrite the code
+    # FIXME: ask if this can be writen in model
+    record.member.name.gsub(record.member.name, "***")
+    # member.name.gsub(/([a-z])+/, '*')
+    # if member.name.size > 4
+    #   member.name.gsub(/([a-z])+/, '*')
+    # elsif member.name.size == 4
+    #   member.name[0,1] + 
+    #   member.name[1,1].gsub(/./, '*') +
+    #   member.name[2,1] +
+    #   member.name[3,1].gsub(/./, '*')
+    # elsif member.name.size == 3
+    #   member.name[0,1] + 
+    #   member.name[1,1].gsub(/./, '*') +
+    #   member.name[2,1]
+    # elsif member.name.size == 2
+    #   member.name[0,1] + 
+    #   member.name[1,1].gsub(/./, '*')
+    # end
+  end
+
 end
