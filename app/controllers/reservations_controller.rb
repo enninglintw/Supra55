560e772896bd4a53c55d0da2b4d14aa634b7e79e
playpool513 <playpool513@gmail.com>
2014-10-04 22:38:32 +0800

    MVC for reservations#new, create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,35 @@
 class ReservationsController < ApplicationController
 
   def welcome
-    
   end
 
   def index
     @reservations = Reservation.all.order(start_at: :asc)
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.member_id = current_user.member.id
+    # FIXME: figure out how to decide section_type
+    @reservation.section_type = "undefined"
+
+    if @reservation.save
+      redirect_to reservations_path, :notice => '新增預約成功！'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_at,
+                                        :end_at)
   end
   
 end
