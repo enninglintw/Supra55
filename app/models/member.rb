b05f7a30a46b84d4dd4a975813aa6e135f3bc970
playpool513 <playpool513@gmail.com>
2014-11-22 15:05:19 +0800

    fine-tune

--- app/models/member.rb
+++ app/models/member.rb
@@ -11,9 +11,21 @@ class Member < ActiveRecord::Base
   as_enum :ebsd_license,     ？: 1, 不具學習資格: 2, 未學習操作: 3, 已學習操作: 4, 已考照: 5
 
   def name_with_org_name
-    "#{name} (#{org.name})"
+    # 串string的不同方法
+    # method 1
+    [name, ' (', org.name, ')'].join
+
+    # method 2
+    # "#{name} (#{org.name})"
+
+    # method 3
     # self.name + " (" + self.org.name + ")"
+
+    # method 4
+    # name + " (" + org.name + ")"
+
     # FIXME: same result, which is better?
+    # A: method 1 or 2
   end
 
 end
